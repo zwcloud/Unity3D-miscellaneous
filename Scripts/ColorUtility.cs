5030e68319e470be70613b36e963b2461b7c5e94
ZCloud <zwcloud@yeah.net>
2016-02-14 12:08:26 +0800

    Add two useful scripts on Gizmos and Color.

---
+++ Scripts/ColorUtility.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+
+public static class ColorUtility
+{
+    public static uint ToUint(Color32 color)
+    {
+        return (uint)((color.a << 24) | (color.r << 16) | (color.g << 8) | (color.b << 0));
+    }
+
+    public static Color32 FromUint(uint color)
+    {
+        return new Color32((byte)(color >> 16), (byte)(color >> 8), (byte)color, (byte)(color >> 24));
+    }
+
+    public static float[] ToHSV(Color32 color)
+    {
+        var r = color.r / 255f;
+        var g = color.g / 255f;
+        var b = color.b / 255f;
+
+        var min = Mathf.Min(r, g, b);
+        var max = Mathf.Max(r, g, b);
+        var delta = max - min;
+
+        var h = 0f;
+        var s = 0f;
+        var v = max;
+
+        if (delta.Equals(0f))
+        {
+            return new[] { h, s, v };
+        }
+
+        s = delta / max;
+
+        var dR = ((max - r) / 6f + delta / 2f) / delta;
+        var dG = ((max - g) / 6f + delta / 2f) / delta;
+        var dB = ((max - b) / 6f + delta / 2f) / delta;
+
+        if (r.Equals(max))
+        {
+            h = dB - dG;
+        }
+        else if (g.Equals(max))
+        {
+            h = 1f / 3f + dR - dB;
+        }
+        else if (b.Equals(max))
+        {
+            h = 2f / 3f + dG - dR;
+        }
+
+        if (h < 0)
+        {
+            h += 1;
+        }
+        else if (h > 1)
+        {
+            h -= 1;
+        }
+
+        return new[] { h, s, v };
+    }
+
+    public static Color32 FromHSV(float h, float s, float v)
+    {
+        if (s.Equals(0f))
+        {
+            var normV = (byte)(v * 255f);
+            return new Color32(normV, normV, normV, 255);
+        }
+
+        h = h.Equals(1f) ? 0f : h * 6f;
+
+        var i = (int)h;
+
+        var r = v;
+        var g = v;
+        var b = v;
+
+        switch (i)
+        {
+            case 0:
+                g = v * (1f - s * (1f - (h - i)));
+                b = v * (1f - s);
+                break;
+            case 1:
+                r = v * (1f - s * (h - i));
+                b = v * (1f - s);
+                break;
+            case 2:
+                r = v * (1f - s);
+                b = v * (1f - s * (1f - (h - i)));
+                break;
+            case 3:
+                r = v * (1f - s);
+                g = v * (1f - s * (h - i));
+                break;
+            case 4:
+                r = v * (1f - s * (1f - (h - i)));
+                g = v * (1f - s);
+                break;
+            case 5:
+                g = v * (1f - s);
+                b = v * (1f - s * (h - i));
+                break;
+        }
+
+        return new Color32((byte)(r * 255f), (byte)(g * 255f), (byte)(b * 255f), 255);
+    }
+}
---
+++ Scripts/GizmosEx.cs
@@ -0,0 +1,70 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+public static class GizmosEx
+{
+    public static bool dir = true;
+    public static long factor;
+    public static float Factor
+    {
+        get
+        {
+            var factor0 = System.DateTime.Now.Ticks % 1000;
+            var factor1 = 1000 - factor0;
+            if (factor == 0)
+            {
+                dir = !dir;
+            }
+
+            factor = dir ? factor0 : factor1;
+
+            return factor / 1000f;
+        }
+    }
+
+    public static float GetGizmoSize(Vector3 position)
+    {
+        Camera current = Camera.current;
+        position = Gizmos.matrix.MultiplyPoint(position);
+
+        float gizmoSize = 1f;
+        if (current)
+        {
+            Transform transform = current.transform;
+            Vector3 position2 = transform.position;
+            float z = Vector3.Dot(position - position2, transform.TransformDirection(new Vector3(0f, 0f, 1f)));
+            Vector3 a = current.WorldToScreenPoint(position2 + transform.TransformDirection(new Vector3(0f, 0f, z)));
+            Vector3 b = current.WorldToScreenPoint(position2 + transform.TransformDirection(new Vector3(1f, 0f, z)));
+            float magnitude = (a - b).magnitude;
+            gizmoSize = 40f / Mathf.Max(magnitude, 0.0001f);
+            gizmoSize = Mathf.Clamp(gizmoSize, 1, 3);
+        }
+
+        return gizmoSize;
+    }
+
+    public static void DrawArrow(Vector3 pos, Vector3 direction, bool flash, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f)
+    {
+        var orginalColor = Gizmos.color;
+        if (flash)
+        {
+            var hsv = ColorUtility.ToHSV(orginalColor);
+            Gizmos.color = ColorUtility.FromHSV(hsv[0], hsv[1], hsv[2] * Factor);
+        }
+
+        var gizmoSize = GetGizmoSize(pos);
+
+        Gizmos.DrawRay(pos, direction * gizmoSize * 2);
+
+        Vector3 right = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 + arrowHeadAngle, 0) * new Vector3(0, 0, 1);
+        Vector3 left = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 - arrowHeadAngle, 0) * new Vector3(0, 0, 1);
+        Gizmos.DrawRay(pos + direction * gizmoSize * 2, right * arrowHeadLength);
+        Gizmos.DrawRay(pos + direction * gizmoSize * 2, left * arrowHeadLength);
+
+        Gizmos.color = orginalColor;
+
+        SceneView.RepaintAll();
+    }
+}
+#endif
